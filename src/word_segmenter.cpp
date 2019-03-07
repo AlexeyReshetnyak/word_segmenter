4b630c7d64dc50c63284fdb4e5684dcc44255d77
Alexey Reshetnyak <alexey.reshetnyak@yandex.ru>
2019-03-07 17:20:37 +0300

    Added filter.

--- src/word_segmenter.cpp
+++ src/word_segmenter.cpp
@@ -1,5 +1,24 @@
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
 int main()
 {
+  string file_name = "../images/006_ND_1947-01-08_006.tif";
+  Mat image = imread(file_name);
+  Mat gray_img(image.rows, image.cols, CV_8UC1);
+  cvtColor(image, gray_img, COLOR_BGR2GRAY);
+  Mat filtered_img(image.rows, image.cols, CV_8UC1);
+  GaussianBlur(gray_img, filtered_img, Size(41, 41), 0, 0);
+  imshow("Source image: ", image);
+  imshow("Gray image: ", gray_img);
+  imshow("Filtered image: ", filtered_img);
+  waitKey(0);
+
   return 0;
 }
 
