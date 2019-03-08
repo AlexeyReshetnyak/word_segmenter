73b4228e880dd52e93efc246f8d610d2d0ca51ee
Alexey Reshetnyak <alexey.reshetnyak@yandex.ru>
2019-03-08 16:44:56 +0300

    Added header.

--- src/segmenter.cpp
+++ src/segmenter.cpp
@@ -1,11 +1,4 @@
-#include <stdint.h>
-#include <opencv2/highgui/highgui.hpp>
-#include "opencv2/imgproc/imgproc.hpp"
-#include <opencv2/core/core.hpp>
-#include <string>
-
-using namespace std;
-using namespace cv;
+#include "segmenter.hpp"
 
 static void fill_edges(const Mat &src, Mat &dst, int32_t val, int32_t edge_size)
 {
@@ -16,44 +9,6 @@ static void fill_edges(const Mat &src, Mat &dst, int32_t val, int32_t edge_size)
   copyMakeBorder(src(roi), dst, edge_size, edge_size, edge_size, edge_size,
                  border_type, value);
 }
-
-class segmentation
-{
-public:
-  Size kernel_size;
-  float sig_x;
-  float sig_y;
-  float area_threshold_val;
-  int32_t thresh;
-  int32_t thresh_type;
-  int32_t max_val;
-  int32_t bound_type;
-  int32_t chain_type;
-  int32_t border_type;
-  int32_t border_size;
-  vector<Rect> bound_rects;
-  vector<vector<Point>> contours;
-  vector<Vec4i> hierarchy;
-  Mat binary_img;
-  Mat gray_img;
-  Mat filtered_img;
-  Mat blocks_img;
-  Mat black_boxes_img;
-  Mat img_with_edges;
-  bool process_edges;
-  bool area_threshold;
-
-  segmentation(const Mat &img);
-  segmentation(const Mat &img, const Mat &src_img);
-  void segment();
-  void clear_vectors();
-  void show_result();
-  void show_debug();
-  void fill_black_boxes();
-
-private:
-  Mat in_img;
-};
 
 
 segmentation::segmentation(const Mat &img)
