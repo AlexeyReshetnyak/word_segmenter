bc8615e8f1bc44e7abdf50c8be2a101c78a52ff5
Alexey Reshetnyak <alexey.reshetnyak@yandex.ru>
2019-03-08 16:53:52 +0300

    Formatting.

--- include/segmenter.hpp
+++ include/segmenter.hpp
@@ -1,8 +1,8 @@
 #include <stdint.h>
+#include <string>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/core/core.hpp>
-#include <string>
 
 using namespace cv;
 using namespace std;
@@ -10,6 +10,7 @@ using namespace std;
 class segmenter
 {
 public:
+  Mat black_boxes_img;
   Size kernel_size;
   float sig_x;
   float sig_y;
@@ -21,17 +22,9 @@ public:
   int32_t chain_type;
   int32_t border_type;
   int32_t border_size;
-  vector<Rect> bound_rects;
-  vector<vector<Point>> contours;
-  vector<Vec4i> hierarchy;
-  Mat binary_img;
-  Mat gray_img;
-  Mat filtered_img;
-  Mat blocks_img;
-  Mat black_boxes_img;
-  Mat img_with_edges;
   bool process_edges;
   bool area_threshold;
+  vector<Rect> bound_rects;
 
   segmenter(const Mat &img);
   segmenter(const Mat &img, const Mat &src_img);
@@ -42,6 +35,13 @@ public:
 
 private:
   Mat in_img;
+  Mat binary_img;
+  Mat gray_img;
+  Mat filtered_img;
+  Mat blocks_img;
+  Mat img_with_edges;
+  vector<vector<Point>> contours;
+  vector<Vec4i> hierarchy;
 };
 
 
